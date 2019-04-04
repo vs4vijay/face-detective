b110fb6c0edd687a570c9342aae9c4b9ef80df74
vs4vijay <vs4vijay@gmail.com>
2019-04-05 01:50:22 +0530

    Image resized while showing

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ config = {
 }
 
 
-haar_cascade_face = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+# haar_cascade_face = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+haar_cascade_face = cv2.CascadeClassifier(f'{cv2.data.haarcascades}/haarcascade_frontalface_default.xml')
 
 
 cap = cv2.VideoCapture(0)
@@ -24,20 +25,15 @@ def open_camera(cap):
 
     processed_frame = process_frame(frame)
 
-    cv2.namedWindow(config['name'], cv2.WINDOW_NORMAL)
+    # cv2.namedWindow(config['name'], cv2.WINDOW_NORMAL)
+    processed_frame = cv2.resize(processed_frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
     cv2.imshow('frame', processed_frame)
-
-    # cv2.waitKey(1) == 27
-    # cv.waitKey(0)
 
     key = cv2.waitKey(1)
     if key == 27 or key == ord('q'):
       break
     elif key == ord('s'):
-      cv2.imwrite('image-saved.png', processed_frame)
-
-    # if ( & 0xFF == ord('q')) or (cv2.waitKey(1) & 0xFF == 27):
-    #       break
+      cv2.imwrite('temp/image-saved.png', processed_frame)
 
 def process_frame(image_frame):
   gray_frame = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
