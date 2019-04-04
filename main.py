f6ad9df88cf74d998e6a5f9b7d2666dd594ed17f
vs4vijay <vs4vijay@gmail.com>
2019-04-05 00:13:45 +0530

    break into functions

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
 def open_camera(cap):
-	while(True):
-	    ret, frame = cap.read()
+  while(True):
+    ret, frame = cap.read()
 
-	    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    processed_frame = process_frame(frame)
 
-	    gray_frame = cv2.flip(gray_frame, 1)
+    cv2.imshow('frame', processed_frame)
 
-	    cv2.imshow('frame', gray_frame)
+    # cv2.waitKey(1) == 27
+    # cv.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
 
-	    # cv2.waitKey(1) == 27
-	    if cv2.waitKey(1) & 0xFF == ord('q'):
-	        break
+def process_frame(image_frame):
+  gray_frame = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+
+  gray_frame = cv2.flip(gray_frame, 1)
+
+  final_frame = detect_faces(gray_frame)
+  return final_frame
+
+
+def detect_faces(image_frame):
+  return image_frame
+
 
 open_camera(cap)
 
